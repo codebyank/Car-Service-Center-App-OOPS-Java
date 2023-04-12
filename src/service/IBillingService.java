9ca13f43c40520ebc2da9174a6ae976f534ec2c7
Ankit Kumar <ank.engg99@gmail.com>
2023-04-12 17:30:43 +0530

    Update IBillingService.java

--- src/service/IBillingService.java
+++ src/service/IBillingService.java
@@ -3,6 +3,6 @@ package service;
 import java.util.HashMap;
 
 public interface IBillingService {
-    void printAllServices(HashMap<String,Integer> prices);
+   
     void generateBill(String name,HashMap<String,Integer> prices);
 }
